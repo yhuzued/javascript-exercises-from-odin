a5e03985d08ec24b41265f314bb6c08e8ec3d748
yhuzued <yusufmait@gmail.com>
2022-08-15 10:51:59 +0700

    finished exercise tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let celcius = (temperature - 32) * 5/9;
+  return Math.round(celcius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let fahrenheit = (temperature * 9/5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
